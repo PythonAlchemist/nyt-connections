4af02422a81cc66942956f2fbb708244ce63bbdb
PythonAlchemist <christopher.singer.analytics@gmail.com>
2023-10-11 22:19:12 -0400

    first pass chat selection

--- chat.py
+++ chat.py
@@ -1,11 +1,83 @@
-from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
+import openai
+from dotenv import load_dotenv, find_dotenv
+from data.clean import Dataset
+from typing import List
+import os
 
-llm = OpenAI()
-chat_model = ChatOpenAI()
+load_dotenv(find_dotenv())
 
-llm.predict("hi!")
->>> "Hi"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-chat_model.predict("hi!")
->>> "Hi"
+
+class ChatInterface:
+    def __init__(self):
+        self.data = Dataset()
+        self.data = self.data.data.head(100)
+        self.chat_history = [
+            {"role": "system", "content": "You are an AI playing a word game."},
+            {"role": "user", "content": self.create_prompt()},
+        ]
+
+    def create_prompt(self) -> str:
+        # Define the prompt
+        return f"""
+        You as the AI are playing a word game that is all about finding connections between words. The game starts with 16 words on the board and your 
+        job will be to respond with four words that are connected. The four sets for four are not connected in any way. Once a set of four is correctly identified 
+        the words in that set are not used again. Feedback will be provided on your responses to help you improve. 1. Correct, 2. One away 
+        (correct except for one word), or 3. Incorrect.
+        After each feedback you will be able to respond with four words again.
+        The game will end after incorrect guesses of four words. 
+
+        Below is provides examples of the correct categories and word sets indexed by the date. Each date is the set of 16 words.
+        {self.data.to_string()}
+
+        Once the game starts you will be given the 16 words and then you will be able to respond with four words.
+        Remember you are the AI and you are playing the game.
+        """
+
+    def chat(self, message: str) -> None:
+        self.chat_history.append({"role": "user", "content": message})
+
+        completion = openai.ChatCompletion.create(
+            model="gpt-4", messages=self.chat_history
+        )
+        print(completion.choices[0].message)
+        self.chat_history.append(completion.choices[0].message)
+
+        return completion.choices[0].message
+
+    def start_game(self, words: List[str]):
+        self.chat_history.append(
+            {
+                "role": "user",
+                "content": f"This is the start of the game, here are the words: {words}. Please respond with four words.",
+            }
+        )
+
+        while True:
+            message = input("Enter your response: ")
+            self.chat_history.append({"role": "user", "content": message})
+            resp = self.chat(message)
+
+
+if __name__ == "__main__":
+    words = [
+        "CAN",
+        "BUTCHER",
+        "ACTION",
+        "LEAD",
+        "PRIME",
+        "CUT",
+        "WAX",
+        "MAY",
+        "TOILET",
+        "TOP",
+        "MIGHT",
+        "CAMERA",
+        "HEAD",
+        "LIGHTS",
+        "SCRAP",
+        "COULD",
+    ]
+    chat = ChatInterface()
+    chat.start_game(words)
--- data/clean.py
+++ data/clean.py
@@ -2,21 +2,27 @@ import pandas as pd
 from datetime import datetime
 
 
-class Dateset:
-    def __init__(self, path):
-        self.path = path
+class Dataset:
+    def __init__(self):
         self.data = self.load_data()
 
-    def load_data(self):
-        df = pd.read_csv(self.path)
-        df["date"] = df["date"].apply(lambda x: convert_date(x))
+    def load_data(self) -> pd.DataFrame:
+        data, date = [], None
+        f = open("data/connections.csv", "r")
+        for line in f:
+            line = line.strip().split(",")
+            if line[0].startswith("NYT Connections"):
+                date = f"{line[1]}, {line[2]}"
+            else:
+                cat = line[0]
+                w1, w2, w3, w4 = line[1], line[2], line[3], line[4]
+                data.append([date, cat, w1, w2, w3, w4])
+        f.close()
+        df = pd.DataFrame(data, columns=["date", "category", "w1", "w2", "w3", "w4"])
+        df["date"] = df["date"].apply(lambda x: self.convert_date(x))
         return df
 
-    def get_data(self, date):
-        return self.data[self.data["date"] == date]
-
-    @staticmethod
-    def convert_date(date_string):
+    def convert_date(self, date_string) -> datetime:
         date, num, year = date_string.split(" ")
 
         num = (
@@ -29,20 +35,5 @@ class Dateset:
         date_string = f"{date} {num}, {year}"
         return datetime.strptime(date_string, "%B %d, %Y")
 
-
-data = []
-date = None
-f = open("data/connections.csv", "r")
-for line in f:
-    line = line.strip().split(",")
-    if line[0].startswith("NYT Connections"):
-        date = f"{line[1]}, {line[2]}"
-    else:
-        cat = line[0]
-        w1, w2, w3, w4 = line[1], line[2], line[3], line[4]
-        data.append([date, cat, w1, w2, w3, w4])
-f.close()
-
-df = pd.DataFrame(data, columns=["date", "category", "w1", "w2", "w3", "w4"])
-df["date"] = df["date"].apply(lambda x: convert_date(x))
-df.to_csv("data/connections_clean.csv", index=False)
+    def write_clean_data(self) -> None:
+        self.data.to_csv("data/connections_clean.csv", index=False)
